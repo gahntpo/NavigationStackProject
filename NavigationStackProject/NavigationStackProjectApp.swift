55370f903b8432755bdc17da0234c387a3d23cd6
Karin Prater <gahntpo@yahoo.de>
2022-11-12 10:51:17 +0200

    Initial Commit

---
+++ NavigationStackProject/NavigationStackProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackProjectApp.swift
+//  NavigationStackProject
+//
+//  Created by Karin Prater on 12.11.22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
